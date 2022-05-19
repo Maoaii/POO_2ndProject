fd1956ab83898e4b632bce3822cb89e0f65e26d2
Maoaii <65407819+Maoaii@users.noreply.github.com>
2022-05-19 15:57:36 +0100

    Output Messages DONE

--- src/versionControlSystem/expections/UserNameAlreadyExistsException.java
+++ src/versionControlSystem/expections/UserNameAlreadyExistsException.java
@@ -1,4 +1,22 @@
 package versionControlSystem.expections;
 
 public class UserNameAlreadyExistsException extends Exception {
+    // Instance variables
+    private String username;
+
+    /**
+     * Exception constructor
+     *
+     * @param username - <code>username</code> that caused the exception
+     */
+    public UserNameAlreadyExistsException(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the <code>username</code> that caused the exception
+     */
+    public String getErrorInfo() {
+        return username;
+    }
 }
